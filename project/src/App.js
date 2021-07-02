fe74b275c2a12ee247074f353332dbb45f57f0e5
Maria Bruner <mariab2898@gmail.com>
2021-07-02 11:24:35 -0400

    geoLocation finished

--- project/src/App.js
+++ project/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Location from './components/Location'
 
--- project/src/components/Location.js
+++ project/src/components/Location.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
+
 class App extends Component {
   constructor(props) {
     super(props);
